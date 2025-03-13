31830b073a76424b25a2ba8073706b3c4ac37cd5
kandavieux221 <kandavieux221@gmail.com>
2025-03-13 14:14:51 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,8 @@
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const randomNumber = getRandomInt(1, 100);
+console.log(randomNumber);
